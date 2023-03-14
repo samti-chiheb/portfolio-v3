73bf8c949578ade82478fd1c60422d7446841ae9
chihebSamti <chiheb.samti@gmail.com>
2023-03-14 18:57:21 +0100

    about us, done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,13 +1,36 @@
 import React from "react";
-import Tilte from "react-tilt";
+import Tilt from "react-tilt";
 import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { services } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
+import { SectionWrapper } from "../hoc";
 
 const ServiceCard = ({ index, title, icon }) => {
-  console.log(title);
-  return <p>{title}</p>;
+  return (
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div
+        variants={fadeIn
+        ("right", "spring", 0.5 * index)}
+        className= "w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+      >
+        <div 
+          options={{
+            max : 45,
+            scale: 1,
+            speed: 450
+          }}
+          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+        >
+          <img className="w-16 h-16 object-contain" src={icon} alt={title} />
+          <h3 className="text-white text-[20px] font-bold text-center">
+            {title}
+          </h3>
+        </div>
+
+      </motion.div>
+    </Tilt>
+  )
 };
 
 const About = () => {
@@ -36,4 +59,4 @@ const About = () => {
   );
 };
 
-export default About;
+export default SectionWrapper(About, "about");
